6f9fe363a725ded97265aba2c57c82f99a5a6b4a
Didier Stadelmann <didier.stadelmann@example.com>
2026-03-21 19:51:44 +0100

    feat(domain): establish immutable data structures for digital twin state

---
+++ hexaplanner/lib/hexaplanner/domain/job.ex
@@ -0,0 +1,22 @@
+defmodule HexaPlanner.Domain.Job do
+  @moduledoc "A task to be scheduled."
+  use Ecto.Schema
+
+  @type t :: %__MODULE__{
+          id: integer() | nil,
+          duration: integer() | nil,
+          required_resources: list(integer()) | nil,
+          start_time: integer() | nil,
+          inserted_at: NaiveDateTime.t() | nil,
+          updated_at: NaiveDateTime.t() | nil
+        }
+
+  @primary_key {:id, :id, autogenerate: true}
+  schema "jobs" do
+    field :duration, :integer # in minutes/ticks
+    field :required_resources, {:array, :integer} # IDs of needed resources
+    # Planning Variables (to be filled by Rust)
+    field :start_time, :integer
+    timestamps()
+  end
+end
---
+++ hexaplanner/lib/hexaplanner/domain/problem.ex
@@ -0,0 +1,10 @@
+defmodule HexaPlanner.Domain.Problem do
+  @moduledoc "The root aggregate holding the entire Twin state."
+  defstruct [:id, resources: [], jobs: []]
+
+  @type t :: %__MODULE__{
+    id: String.t() | nil,
+    resources: list(HexaPlanner.Domain.Resource.t()),
+    jobs: list(HexaPlanner.Domain.Job.t())
+  }
+end
