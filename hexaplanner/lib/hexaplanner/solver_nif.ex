cf5298f15ba3068260ba20afd24087fc603bc4c0
Didier Stadelmann <didier.stadelmann@example.com>
2026-03-25 07:26:17 +0100

    refactor: rename Rust crate to hexacore_engine and fix test aliases (Phase 2)

--- hexaplanner/lib/hexaplanner/solver_nif.ex
+++ hexaplanner/lib/hexaplanner/solver_nif.ex
@@ -3,7 +3,7 @@ defmodule HexaPlanner.SolverNif do
   NIF bridge to the Rust implementation.
   """
 
-  use Rustler, otp_app: :hexaplanner, crate: "hexa_solver"
+  use Rustler, otp_app: :hexaplanner, crate: "hexacore_engine"
 
   @spec add(integer(), integer()) :: integer()
   def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
